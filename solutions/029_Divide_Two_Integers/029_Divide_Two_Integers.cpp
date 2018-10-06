54ac39733109bb8a805a743f1a3777d4c415043f
johnnysun <johnnysuns@163.com>
2018-10-06 15:21:46 +0800

    029 readme

---
+++ solutions/029_Divide_Two_Integers/029_Divide_Two_Integers.cpp
@@ -0,0 +1,73 @@
+// Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+//
+// Return the quotient after dividing dividend by divisor.
+//
+// The integer division should truncate toward zero.
+//
+// Example 1:
+//
+//
+// Input: dividend = 10, divisor = 3
+// Output: 3
+//
+// Example 2:
+//
+//
+// Input: dividend = 7, divisor = -3
+// Output: -2
+//
+// Note:
+//
+//
+// 	Both dividend and divisor will be 32-bit signed integers.
+// 	The divisor will never be 0.
+// 	Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
+//
+//
+
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int sgn=0,res=0;
+        if(dividend==INT_MIN){
+            if(divisor==-1) return INT_MAX;
+            else if(divisor==1) return INT_MIN;
+        }
+        if(divisor==INT_MIN) {
+            if(dividend==INT_MIN)return 1;
+            else return 0;
+        }
+        if((dividend<0)^(divisor<0)){
+            sgn=1;
+        }
+        divisor=abs(divisor);
+        if(dividend==INT_MIN){
+            dividend=INT_MAX;
+            dividend=dividend-divisor+1;
+            res++;
+        }else{
+            dividend=abs(dividend);
+        }
+        int level=1,divisor_tmp=divisor;
+        while(dividend>=divisor){
+            //cout<<"**devidend:"<<dividend<<endl;
+            while((divisor_tmp<0x3FFFFFFF)&&(dividend>=(divisor_tmp<<1))){
+                divisor_tmp=divisor_tmp<<1;
+                level=level<<1;
+                //cout<<"++devidend:"<<dividend<<" divisor_tmp:"<<divisor_tmp<<endl;
+            }
+            //cout<<"res:"<<res<<" dividend:"<<dividend<<endl;
+            dividend-=divisor_tmp;
+            res+=level;
+            //cout<<"start --divisor_tmp:"<<divisor_tmp << " dividend:"<<dividend<<endl;
+            while((divisor_tmp)&&(dividend<divisor_tmp)){
+                //cout<<"--devidend:"<<dividend<<" divisor_tmp:"<<divisor_tmp<<endl;
+                divisor_tmp=divisor_tmp>>1;
+                level=level>>1;
+            }
+        }
+        if(sgn)return -res;
+        else return res;
+    }
+};
---
+++ solutions/029_Divide_Two_Integers/readme.md
@@ -0,0 +1,59 @@
+# 29. Divide Two Integers
+
+## 简析
+
+    模拟整除运算，不准使用乘除。数据范围int32，结果越界就输出边界
+
+## 方法：循环减代替整除
+
+先不考虑越界，直接用循环减除数，发现会超时。可以用位操作快速（指数级增长）增大除数，同时位移整除结果步进，加快被除数被减小的速度。最后再一步步快速减小除数，直到被除数被减到小于除数;
+
+考虑越界有两种做法：
+
+1. 对所有超出边界的情况作特殊处理；
+2. 直接强制使用long long，输出时再判断越界。
+
+**我的代码：**
+
+```cpp
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int sgn=0,res=0;
+        if(dividend==INT_MIN){
+            if(divisor==-1) return INT_MAX;
+            else if(divisor==1) return INT_MIN;
+        }
+        if(divisor==INT_MIN) {
+            if(dividend==INT_MIN)return 1;
+            else return 0;
+        }
+        if((dividend<0)^(divisor<0)){
+            sgn=1;
+        }
+        divisor=abs(divisor);
+        if(dividend==INT_MIN){
+            dividend=INT_MAX;
+            dividend=dividend-divisor+1;
+            res++;
+        }else{
+            dividend=abs(dividend);
+        }
+        int level=1,divisor_tmp=divisor;
+        while(dividend>=divisor){
+            while((divisor_tmp<0x3FFFFFFF)&&(dividend>=(divisor_tmp<<1))){
+                divisor_tmp=divisor_tmp<<1;
+                level=level<<1;
+            }
+            dividend-=divisor_tmp;
+            res+=level;
+            while((divisor_tmp)&&(dividend<divisor_tmp)){
+                divisor_tmp=divisor_tmp>>1;
+                level=level>>1;
+            }
+        }
+        if(sgn)return -res;
+        else return res;
+    }
+};
+```
