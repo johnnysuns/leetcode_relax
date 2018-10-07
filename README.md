7d64fab02efe4034affe60a2e71db126d84a5b3a
johnnysun <johnnysuns@163.com>
2018-10-07 14:22:55 +0800

    033 cpp

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Leetcode Solutions
-Update time:  2018-10-06 16:21:14
+Update time:  2018-10-07 14:20:33
 
 Auto created by leetcode_generate modified from [bonfy/leetcode](https://github.com/bonfy/leetcode)
 
-**32   /   863** problems solved!
+**34   /   867** problems solved!
 
 **139** problems locked.
 
@@ -39,5 +39,7 @@ Auto created by leetcode_generate modified from [bonfy/leetcode](https://github.
 |28|[Implement strStr()](https://leetcode.com/problems/implement-strstr)|[Cpp](solutions/028_Implement_strStr()/028_Implement_strStr().cpp)||Easy|
 |29|[Divide Two Integers](https://leetcode.com/problems/divide-two-integers)|[Cpp](solutions/029_Divide_Two_Integers/029_Divide_Two_Integers.cpp)|[Note](solutions/029_Divide_Two_Integers/readme.md)|Medium|
 |30|[Substring with Concatenation of All Words](https://leetcode.com/problems/substring-with-concatenation-of-all-words)|[Cpp](solutions/030_Substring_with_Concatenation_of_All_Words/030_Substring_with_Concatenation_of_All_Words.cpp)|[Note](solutions/030_Substring_with_Concatenation_of_All_Words/readme.md)|Hard|
+|32|[Longest Valid Parentheses](https://leetcode.com/problems/longest-valid-parentheses)|[Cpp](solutions/032_Longest_Valid_Parentheses/032_Longest_Valid_Parentheses.cpp)||Hard|
+|33|[Search in Rotated Sorted Array](https://leetcode.com/problems/search-in-rotated-sorted-array)|[Cpp](solutions/033_Search_in_Rotated_Sorted_Array/033_Search_in_Rotated_Sorted_Array.cpp)||Medium|
 |34|[Find First and Last Position of Element in Sorted Array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array)|[Cpp](solutions/034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp)|[Note](solutions/034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/readme.md)|Medium|
 |375|[Guess Number Higher or Lower II](https://leetcode.com/problems/guess-number-higher-or-lower-ii)|[Cpp](solutions/375_Guess_Number_Higher_or_Lower_II/375_Guess_Number_Higher_or_Lower_II.cpp)|[Note](solutions/375_Guess_Number_Higher_or_Lower_II/readme.md)|Medium|
---
+++ solutions/033_Search_in_Rotated_Sorted_Array/033_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,66 @@
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+//
+// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+//
+// You are given a target value to search. If found in the array return its index, otherwise return -1.
+//
+// You may assume no duplicate exists in the array.
+//
+// Your algorithm's runtime complexity must be in the order of O(log n).
+//
+// Example 1:
+//
+//
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+//
+//
+// Example 2:
+//
+//
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+//
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target,int l,int r) {
+        int mid_i =(r+l)/2;
+        int mid_v =nums[mid_i];
+        //cout<<"searching l="<<l<<" r="<<r<<endl;
+        if(mid_v==target){
+            return mid_i;
+        }else {
+            if(l<r){
+                if(nums[r]>nums[l]){ //normal
+                    if(mid_v>target) return search(nums, target,l,mid_i);
+                    else return search(nums, target,mid_i+1,r);
+                }else{//pivot
+                    if(nums[l]<=mid_v){//normal left
+                        if(mid_v>target){
+                            if(target>nums[r]) return search(nums, target,l,mid_i);
+                            else return search(nums, target,mid_i+1,r);
+                        }else{
+                            return search(nums, target,mid_i+1,r);
+                        }
+                    }
+                    else{//normal right
+                        if(mid_v>target){
+                            return search(nums, target,l,mid_i);                        
+                        }else{
+                            if(target>nums[r])return search(nums, target,l,mid_i);
+                            else return search(nums, target,mid_i+1,r);
+                        }
+                    }
+                }
+            }else{
+                return -1;
+            }
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        if (nums.size()==0)return -1;
+        return search(nums, target,0,nums.size()-1);
+    }
+};
