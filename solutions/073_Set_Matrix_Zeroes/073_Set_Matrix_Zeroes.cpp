30b4b36e5dbd217da53354474bdfe5cad64996d4
Johnny Sun <johnnysuns@163.com>
2019-07-19 23:22:23 +0800

    073 cpp

---
+++ solutions/073_Set_Matrix_Zeroes/073_Set_Matrix_Zeroes.cpp
@@ -0,0 +1,81 @@
+// Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+//
+// Example 1:
+//
+//
+// Input: 
+// [
+//   [1,1,1],
+//   [1,0,1],
+//   [1,1,1]
+// ]
+// Output: 
+// [
+//   [1,0,1],
+//   [0,0,0],
+//   [1,0,1]
+// ]
+//
+//
+// Example 2:
+//
+//
+// Input: 
+// [
+//   [0,1,2,0],
+//   [3,4,5,2],
+//   [1,3,1,5]
+// ]
+// Output: 
+// [
+//   [0,0,0,0],
+//   [0,4,5,0],
+//   [0,3,1,0]
+// ]
+//
+//
+// Follow up:
+//
+//
+// 	A straight forward solution using O(mn) space is probably a bad idea.
+// 	A simple improvement uses O(m + n) space, but still not the best solution.
+// 	Could you devise a constant space solution?
+//
+//
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        if(m==0) return;
+        int n=matrix[0].size();
+        if(n==0) return;
+        int zeroi[m]={0};//can be stored in firt row
+        int zeroj[n]={0};//can be stored in last col
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    zeroi[i]=1;
+                    zeroj[j]=1;
+                }
+            }
+        }
+        //clear row
+        for(int i=0;i<m;i++){
+            if(zeroi[i]){
+                for(int j=0;j<n;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        //clear col
+        for(int i=0;i<n;i++){
+            if(zeroj[i]){
+                for(int j=0;j<m;j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+    }
+};
