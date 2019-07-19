271c04ca26272863a293e6bd69287b64857caa0f
Johnny Sun <johnnysuns@163.com>
2019-07-19 23:22:15 +0800

    070 cpp

---
+++ solutions/070_Climbing_Stairs/070_Climbing_Stairs.cpp
@@ -0,0 +1,44 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+//
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//
+// Note: Given n will be a positive integer.
+//
+// Example 1:
+//
+//
+// Input: 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+//
+//
+// Example 2:
+//
+//
+// Input: 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+//
+//
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        //fibonacci
+        int a0=1;
+        int a1=1;
+        int a3=1;
+        for(int i=2;i<=n;i++){
+            a3=a0+a1;
+            a0=a1;
+            a1=a3;
+        }
+        return a3;
+    }
+};
